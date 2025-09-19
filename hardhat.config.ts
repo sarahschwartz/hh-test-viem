ec84b12bd297843001f32c180f36372534499137
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-09-19 10:07:09 -0600

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,53 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import { configVariable } from "hardhat/config";
+import "@nomicfoundation/hardhat-viem";
+
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxViemPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  ignition: {
+    requiredConfirmations: 1,
+  },
+  networks: {
+    zksyncOS: {
+      type: "http",
+      chainType: "generic",
+      chainId: 8022833,
+      url: "https://zksync-os-testnet-alpha.zksync.dev",
+      accounts: [configVariable("TESTNET_PRIVATE_KEY")],
+    },
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+  },
+};
+
+export default config;
